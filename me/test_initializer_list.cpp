42a4621121773ae023eda7c4f19d9a9fc0a9f517
Johan <johan@johanswork.com>
2023-03-29 19:15:48 +0200

    water::no_initializer_list test

---
+++ me/test_initializer_list.cpp
@@ -0,0 +1,12 @@
+// Copyright 2023 Johan Paulsson
+// This file is part of the Water C++ Library. It is licensed under the MIT License.
+// See the license.txt file in this distribution or https://watercpp.com/license.txt
+//\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_
+#include "base.hpp"
+#include <water/tests/no_initializer_list.hpp>
+namespace me {
+namespace {
+    auto x = add(
+    );
+}
+}
