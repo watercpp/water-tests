742ee1752388540944afe57c9c2be1b94b4869c5
Johan <johan@johanswork.com>
2023-04-06 18:03:28 +0200

    test water::std_allocator

--- me/test_allocator.cpp
+++ me/test_allocator.cpp
@@ -1,13 +1,15 @@
-// Copyright 2018 Johan Paulsson
+// Copyright 2018-2023 Johan Paulsson
 // This file is part of the Water C++ Library. It is licensed under the MIT License.
 // See the license.txt file in this distribution or https://watercpp.com/license.txt
 //\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_
 #include "base.hpp"
 #include <water/tests/allocator.hpp>
+#include <water/tests/std_allocator.hpp>
 namespace me {
 namespace {
     auto x = add(
-        water::tests::allocator_all
+        water::tests::allocator_all,
+        water::tests::std_allocator_all
     );
 }
 }
